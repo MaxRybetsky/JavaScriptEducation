e9d3e53978ebd56db7cff12d1250cd58abe1cf62
MaxRybetsky <stsdewar@ya.ru>
2020-12-19 23:26:56 +0300

    Functions in JS

--- start/script.js
+++ start/script.js
@@ -1,12 +1,29 @@
-let answers = [];
-let questions = [
-    'What is your name?',
-    'What is your surname?',
-    'How old are you?'
-];
+function myFirstApp(name, age) {
+    console.log(`Hello ${name}!`);
 
-for(let i = 0; i < questions.length; i++) {
-    answers[i] = prompt(questions[i], '');
+    showSkills();
+    checkAge();
+    calcPow(4);
+
+    function showSkills() {
+        let skills = ['html', 'css', 'js'];
+        for (let i = 0; i < skills.length; i++) {
+            console.log(skills[i]);
+        }
+    }
+
+    function checkAge() {
+        if(age > 18) {
+            console.log("You're good one!");
+        } else {
+            console.log("Hmm, you are the best!!!");
+        }
+    }
+
+    function calcPow(num) {
+        num *= num;
+        console.log(num);
+    }
 }
 
-document.write(answers);
+myFirstApp('Max', 24);
