3f3c3ca53c36310c8636b2bbeb14777b25bfb0b6
MaxRybetsky <stsdewar@ya.ru>
2020-12-19 12:25:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your surname?', '');
+answers[2] = prompt('How old are you?', '');
+
+document.write(answers);
+
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+}
