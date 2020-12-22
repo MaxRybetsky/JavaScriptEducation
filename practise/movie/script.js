c657ebd63b99655f361f79c474065da55ae410c3
MaxRybetsky <stsdewar@ya.ru>
2020-12-22 17:13:37 +0300

    Upgrate task with loops

--- practise/movie/script.js
+++ practise/movie/script.js
@@ -1,4 +1,16 @@
-const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+"use strict";
+
+let numberOfFilms;
+
+let correct = false;
+do {
+  numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+  if (numberOfFilms < 0) {
+    alert("Количество указано неверно!");
+  } else {
+    correct = true;
+  }
+} while (!correct);
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,12 +20,31 @@ const personalMovieDB = {
   private: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов", ""),
-  b = prompt("Насколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов", ""),
-  d = prompt("Насколько оцените его?", "");
+for (let i = 0; i < numberOfFilms; i++) {
+  const movie = prompt("Один из последних просмотренных фильмов", ""),
+    rating = +prompt("Насколько оцените его?", "");
+  if (
+    movie != null &&
+    movie.length != 0 &&
+    rating > 0 &&
+    rating < 10 &&
+    movie.length <= 50
+  ) {
+    personalMovieDB.movies[movie] = rating;
+  } else {
+    i--;
+    alert("Данные введены неверно - попробуйте еще раз!");
+  }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (numberOfFilms < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+  alert("Вы классический зритель");
+} else if (numberOfFilms > 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка!");
+}
 
 console.log(personalMovieDB);
